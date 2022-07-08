6dabcb20b1c7a3f13beaeaa7f1c00af95563e25b
Varis Vitols <varisvitols@gmail.com>
2022-07-08 10:44:57 +0300

    Setting up categories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 2,
+      title: 'Jackets'
+    },
+    {
+      id: 3,
+      title: 'Sneakers'
+    },
+    {
+      id: 4,
+      title: 'Womens'
+    },
+    {
+      id: 5,
+      title: 'Mens'
+    },
+  ]
   return (
-    <div>
-      Hello world
+    <div className="categories-container">
+      {categories.map(({title}) => (
+        <div className="category-container">
+          {/*<img /> */}
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
