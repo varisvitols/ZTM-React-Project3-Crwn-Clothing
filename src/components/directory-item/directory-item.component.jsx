de3123927e962b846241829dfc96ffef8484d33b
Varis Vitols <varisvitols@gmail.com>
2022-09-17 22:45:32 +0300

    Navigation in Directory (home) component, some refactoring

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,17 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+
 import { DirectoryItemContainer, BackgroundImage, Body } from './directory-item.styles';
 
-const DirectoryItem = ({ category: { imageUrl, title} }) => {
-    return (
-        <DirectoryItemContainer>
-          <BackgroundImage imageUrl={imageUrl} style={{
-            backgroundImage: `url(${imageUrl})`
-          }} />
-          <Body>
-            <h2>{title}</h2>
-            <p>Shop now</p>
-          </Body>
-        </DirectoryItemContainer>
-    )
+const DirectoryItem = ({ category: { imageUrl, title, route } }) => {
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+
+  return (
+      <DirectoryItemContainer onClick={onNavigateHandler}>
+        <BackgroundImage imageUrl={imageUrl} style={{
+          backgroundImage: `url(${imageUrl})`
+        }} />
+        <Body>
+          <h2>{title}</h2>
+          <p>Shop now</p>
+        </Body>
+      </DirectoryItemContainer>
+  )
 }
 
 export default DirectoryItem
