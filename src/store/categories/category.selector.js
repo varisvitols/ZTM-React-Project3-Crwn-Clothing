e0f1b923a7cfdd95b47df2d0a3ffd75f32cbb55a
Varis Vitols <varisvitols@gmail.com>
2022-09-21 21:48:53 +0300

    Reselect added to the project to memoize Category selection

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,9 +1,37 @@
-export const selectCategoriesMap = (state) => {
-    console.log('categories selector fired');
-    return state.categories.categories
-        .reduce((acc, category) => {
+import { createSelector } from 'reselect'
+
+const selectCategoryReducer = (state) => {
+    console.log('Selector 1 fired');
+    return state.categories;
+}
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => {
+        console.log('Selector 2 fired');
+        return categoriesSlice.categories;
+    }
+)
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => {
+        console.log('Selector 3 fired');
+        return categories.reduce((acc, category) => {
             const { title, items } = category;
             acc[title.toLowerCase()] = items;
             return acc;
         }, {})
-}
+    }
+)
+
+
+// export const selectCategoriesMap = (state) => {
+//     console.log('categories selector fired');
+//     return state.categories.categories
+//         .reduce((acc, category) => {
+//             const { title, items } = category;
+//             acc[title.toLowerCase()] = items;
+//             return acc;
+//         }, {})
+// }
