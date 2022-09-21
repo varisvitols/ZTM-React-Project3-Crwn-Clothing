bf131c8fb845a6c66d6a4d31344e6cc4ad2b4ebc
Varis Vitols <varisvitols@gmail.com>
2022-09-21 20:51:59 +0300

    Custom logger middleware created

--- src/store/store.js
+++ src/store/store.js
@@ -3,8 +3,33 @@ import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
+// Function generator
+// const curryFunc = (a) => (b, c) => {
+//     return a + b - c
+// }
+// const with3 = curryFunc(3);
+// const with10 = curryFunc(10);
+// with10(9, 2);
+// with3(2, 4);
+// Middleware is a reusable function
+
+const loggerMiddleware = (store) => (next) => (action) => {
+    if (!action.type) {
+        return next(action);
+    }
+
+    console.log('CM:type: ', action.type);
+    console.log('CM:payload: ', action.payload);
+    console.log('CM:currentState: ', store.getState());
+
+    next(action);
+
+    console.log('CM:next state: ', store.getState());
+}
+
 // Middlewares in redux are executed just before they hit the reducers.
-const middleWares = [logger];
+// const middleWares = [logger];
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
